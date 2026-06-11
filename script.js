e8babdebba808ed9467cdcece713f60be17bb7b3
Ilya <jjcuken650@gmail.com>
2026-06-11 13:34:08 +0300

    add renderBoard function for displaying tasks

--- script.js
+++ script.js
@@ -21,9 +21,39 @@ document.addEventListener("DOMContentLoaded", () => {
   const modalWrapper = document.getElementById("modal-wrapper");
   const taskForm = document.getElementById("task-form");
 
+  const todoZone = document.getElementById("todo-zone");
+  const inprogressZone = document.getElementById("inprogress-zone");
+  const doneZone = document.getElementById("done-zone");
+
+  const todoAmount = document.getElementById("to-do-amount");
+  const inprogressAmount = document.getElementById("in-progress-amount");
+  const doneAmount = document.getElementById("done-amount");
+  
   let tasks = [];
   let currentSelectedPriority = 'medium';
+  
 
+  btnAdd.addEventListener("click", () => {
+    modalWrapper.classList.add("open");
+  });
+
+  btnCancel.addEventListener("click", closeModal);
+
+  priorityButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      priorityButtons.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
+
+      currentSelectedPriority = btn.dataset.priority;
+    });
+  });
+
+  taskForm.addEventListener("submit", event => {
+    event.preventDefault();
+    addTask();
+    closeModal();
+    renderBoard();
+  });
 
   function closeModal() {
     modalWrapper.classList.remove("open");
@@ -46,27 +76,40 @@ document.addEventListener("DOMContentLoaded", () => {
     tasks.push(newTask);
   }
 
+  function renderBoard() {
+    todoZone.innerHTML = "";
+    inprogressZone.innerHTML = "";
+    doneZone.innerHTML = "";
 
-  btnAdd.addEventListener("click", () => {
-    modalWrapper.classList.add("open");
-  });
+    let countTodo = 0;
+    let countInProgress = 0;
+    let countDone = 0;
 
-  btnCancel.addEventListener("click", closeModal);
-
-  priorityButtons.forEach(btn => {
-    btn.addEventListener("click", () => {
-      priorityButtons.forEach(b => b.classList.remove("active"));
-      btn.classList.add("active");
-
-      currentSelectedPriority = btn.dataset.priority;
+    tasks.forEach(task => {
+      const cardHTML = `
+      <div class="task-card" draggable="true" data-id="${task.id}">
+        <span class="badge badge-${task.priority}">${task.priority}</span>
+        <h3>${task.title}</h3>
+        <p>${task.description}</p>
+        <div class="card-actions">
+          <button class="btn-edit" data-id="${task.id}">Edit</button>
+          <button class="btn-delete" data-id="${task.id}">Delete</button>
+        </div>
+      </div>
+    `;
+      if (task.column === "todo") {
+        todoZone.insertAdjacentHTML("beforeend", cardHTML);
+        countTodo++;
+      } else if (task.column === "inprogress") {
+        inprogressZone.insertAdjacentHTML("beforeend", cardHTML);
+        countInProgress++;
+      } else {
+        doneZone.insertAdjacentHTML("beforeend", cardHTML);
+        countDone++;
+      }
     });
-  });
-
-  taskForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    addTask();
-    closeModal();
-  });
-
-
+    todoAmount.textContent = countTodo;
+    inprogressAmount.textContent = countInProgress;
+    doneAmount.textContent = countDone;
+  }
 });
